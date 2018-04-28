8bc4a98ae7805cfb5b720df99d989b1de6bf9f42
Serov <serov-pa@yandex.ru>
2018-04-28 14:32:38 +0300

    add migrate

--- data/Migrations/Version20180426135325.php
+++ data/Migrations/Version20180426135325.php
@@ -21,21 +21,55 @@ class Version20180426135325 extends AbstractMigration
 
     public function up(Schema $schema)
     {
-        $table = $schema->createTable('post');
+        /**
+         * Категории
+         */
+        $table = $schema->createTable('category');
         $table->addColumn('id', 'integer', ['autoincrement'=>true]);
-        $table->addColumn('title', 'text', ['notnull'=>true]);
-        $table->addColumn('content', 'text', ['notnull'=>true]);
-        $table->addColumn('status', 'integer', ['notnull'=>true]);
-        $table->addColumn('status2', 'integer', ['notnull'=>true]);
-        $table->addColumn('date_created', 'datetime', ['notnull'=>true]);
+        $table->addColumn('category_key', 'string',['notnull'=>false,'length'=>128]);
+        $table->addColumn('category_name', 'string',['notnull'=>false,'length'=>128]);
+        $table->addColumn('date_created', 'datetime', ['notnull'=>false]);
         $table->setPrimaryKey(['id']);
+        $table->addUniqueIndex(['category_key'], 'category_key', [],[]);
+        $table->addOption('engine' , 'InnoDB');
+
+        /**
+         * ПОсты
+         */
+        $table = $schema->createTable('article');
+        $table->addColumn('id', 'integer', ['autoincrement'=>true]);
+        $table->addColumn('category_id', 'integer',['notnull'=>false]);
+        $table->addColumn('title', 'string',['notnull'=>false,'length'=>100]);
+        $table->addColumn('article', 'string',['notnull'=>false,'length'=>255]);
+        $table->addColumn('short_article', 'string',['notnull'=>false,'length'=>255]);
+        $table->addColumn('is_public', 'boolean',['notnull'=>false, 'default'=>0]);
+        $table->addColumn('date_created', 'datetime', ['notnull'=>false]);
+        $table->setPrimaryKey(['id']);
+        $table->addIndex(['category_id'], 'category_id_index');
+        $table->addForeignKeyConstraint('category', ['category_id'], ['id'], ['onUpdate'=>'CASCADE'], 'post_category_id_fk');
+        $table->addOption('engine' , 'InnoDB');
+
+        /**
+         * Комментарии
+         */
+        $table = $schema->createTable('comment');
+        $table->addColumn('id', 'integer', ['autoincrement'=>true]);
+        $table->addColumn('article_id', 'integer',['notnull'=>false,'length'=>128]);
+        $table->addColumn('user_email', 'string',['notnull'=>false,'length'=>128]);
+        $table->addColumn('comment', 'string',['notnull'=>false,'length'=>128]);
+        $table->addColumn('date_created', 'datetime', ['notnull'=>false]);
+        $table->setPrimaryKey(['id']);
+        $table->addIndex(['article_id'], 'article_id');
+        $table->addForeignKeyConstraint('article', ['article_id'], ['id'], ['onUpdate'=>'CASCADE'], 'comment_article_id_fk');
         $table->addOption('engine' , 'InnoDB');
 
     }
 
     public function down(Schema $schema)
     {
-        $schema->dropTable('post');
+        $schema->dropTable('category');
+        $schema->dropTable('article');
+        $schema->dropTable('comment');
 
     }
 }
